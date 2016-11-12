4ea5d0970d0fbde938718602db54209589426a69
turt2live <travpc@gmail.com>
2016-11-11 23:05:19 -0700

    Base structure

---
+++ src/ents-makerspace-forms.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Class Am_Plugin_EntsMakerspaceForms
+ */
+class Am_Plugin_EntsMakerspaceForms extends Am_Plugin
+{
+    const PLUGIN_STATUS = self::STATUS_PRODUCTION;
+    const PLUGIN_COMM = self::COMM_FREE;
+    const PLUGIN_REVISION = "1.0.0";
+
+}
